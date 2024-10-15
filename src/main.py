8d1a72ba994b29fffc16e52ab2d404a96fc047ce
darronese <darron198@hotmail.com>
2024-10-14 17:55:45 -0700

    Added bare bones chords and notes classes. Testing and messing around with python

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
-from music21 import environment
+from header import *
 
-print(environment.UserSettings()['musicxmlPath'])
+#user settings
+us = environment.UserSettings()
+#where my musescore is located
+us['musicxmlPath'] = '/Applications/MuseScore 4.app/'
 
 
+#TO DO:
+#1: Implement rest notes
+#2: Get acutal generation of notes going.
+def main():
+
+
+    main()
